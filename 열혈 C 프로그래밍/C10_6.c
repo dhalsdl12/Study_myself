079b1d5cf8b4c622143b0b34671f5fb14156bc3f
ohmin <71929898+dhalsdl12@users.noreply.github.com>
2020-12-18 22:48:36 +0900

    C 10-6

---
+++ 열혈 C 프로그래밍/C10_6.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	int time;
+	int second, min, hour;
+
+	printf("ÃÊ(second) ÀÔ·Â: ");
+	scanf("%d", &time);
+
+	second = time%60;
+	min = (time/60)%60;
+	hour = (time/60)/60;
+
+	printf("[h: %d, m: %d, s: %d]\n", hour, min, second);
+}
