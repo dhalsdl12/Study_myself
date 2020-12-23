bbf0fc641b6dfdd2d68ede5cb18578ac8ddbd0c6
ohmin <71929898+dhalsdl12@users.noreply.github.com>
2020-12-23 22:47:25 +0900

    C 13-1-4

---
+++ 열혈 C 프로그래밍/C13_1_4.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+	int arr[6] = {1,2,3,4,5,6};
+	int *fptr = arr;
+	int *bptr = &arr[5];
+	int i, temp;
+
+	for(i = 0; i < 6/3; i++)
+	{
+		temp = *fptr;
+		*fptr = *bptr;
+		*bptr = temp;
+
+		fptr++;
+		bptr--;
+	}
+
+	for(i = 0; i < 6; i++)
+		printf("%d  ", arr[i]);
+
+	return 0;
+}
