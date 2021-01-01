3ed1b8de51bdb344fb6535b3fb7853e13a2c78d3
ohmin <71929898+dhalsdl12@users.noreply.github.com>
2021-01-01 17:41:55 +0900

    C - getchar putchar

---
+++ 따즈아 C/Cgetcharputchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+	//getchar(), putchar()
+	//한문자만 입력시 사용
+	//buffer : 데이터의 임시저장 공간
+	char i, j, k;
+
+	i = getchar();
+	putchar(i);
+	getchar();
+
+	j = getchar();
+	putchar(j);
+	getchar();
+
+	k = getchar();
+	putchar(k);
+	
+}
