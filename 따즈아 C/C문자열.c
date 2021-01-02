64102fec82bcb607d548b463c9aaf81c423732e9
ohmin <71929898+dhalsdl12@users.noreply.github.com>
2021-01-02 23:24:41 +0900

    C - 문자열

---
+++ 따즈아 C/C문자열.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+void string_copy(char arr_1, char arr_2);
+int main()
+{
+	//char str1[] = "hello";
+	//char str2[] = { 'h','e','l','l','o' };
+
+	//printf("sizeof(srt1): %d\n", sizeof(str1));
+	//printf("sizeof(srt2): %d\n", sizeof(str2));
+
+	char country[10] = "Korea";
+	char nation[10];
+
+	strcpy(nation, country);
+
+}
+
+void string_copy()
+{
+
+}
