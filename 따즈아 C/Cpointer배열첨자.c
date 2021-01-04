ea055bd0e1658f28622675d93f74e0b549765732
ohmin <71929898+dhalsdl12@users.noreply.github.com>
2021-01-04 23:11:38 +0900

    C - 배열 첨자

---
+++ 따즈아 C/Cpointer배열첨자.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+	int ar[4] = { 10,20,30,40 };
+	int *par;
+	par = ar;//&ar[0]
+	//*(par+0) == ar[0]
+
+	printf("*(par+0) = %d\n", *(par + 0));
+	printf("par[0] = %d\n", par[0]);
+	printf("par[1] = %d\n", par[1]);
+	printf("par[2] = %d\n", par[2]);
+	printf("par[3] = %d\n", par[3]);
+
+	//배열명[0] == *(배열명+0)  동일!!!
+}
