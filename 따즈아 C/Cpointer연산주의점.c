6aca0505ff5e2eff051a8b2a50cc03dd45caaf8d
ohmin <71929898+dhalsdl12@users.noreply.github.com>
2021-01-04 23:13:00 +0900

    Add files via upload

---
+++ 따즈아 C/Cpointer연산주의점.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main()
+{
+	int x = 100;
+	int *px = &x;
+
+	//first
+	printf("x : %d\n", x);
+	printf("&x : %d\n", &x);
+	printf("*px : %d\n", *px);
+	printf("px : %d\n\n", px);
+
+	//1) px++
+	px++;
+	printf("x : %d\n", x);
+	printf("&x : %d\n", &x);
+	printf("*px : %d\n", *px);
+	printf("px : %d\n\n", px);
+
+	//2) *px++
+	px = &x;
+	printf("x : %d\n", x);
+	printf("&x : %d\n", &x);
+	printf("*px : %d\n", *px);
+	printf("*px++ : %d\n", *px++); //px가 가지고있는 값을 출력하고 ++한다.
+	printf("px : %d\n\n", px);
+
+	//3) (*px)++
+	px = &x;
+	(*px)++;
+	printf("x : %d\n", x);
+	printf("&x : %d\n", &x);
+	printf("*px : %d\n", *px);
+	printf("px : %d\n\n", px);
+}
