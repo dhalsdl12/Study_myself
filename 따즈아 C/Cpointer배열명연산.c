fdc32f28bef46553b667ea81a2c718d42fbd5217
ohmin <71929898+dhalsdl12@users.noreply.github.com>
2021-01-04 23:11:05 +0900

    C - pointer 배열명 연산

---
+++ 따즈아 C/Cpointer배열명연산.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	int a[3] = { 10,20,30 };
+
+	printf("a[0] = %d\n", a[0]);
+	printf("*a = %d\n", *a);
+	printf("a[1] = %d\n", a[1]);
+	printf("*(a+1) = %d\n", *(a + 1));
+	printf("a[2] = %d\n", a[2]);
+	printf("*(a+2) = %d\n", *(a + 2));
+}
