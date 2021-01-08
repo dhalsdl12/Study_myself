f020d9f0ccac8f137e510315506f81d8808f6470
ohmin <71929898+dhalsdl12@users.noreply.github.com>
2021-01-08 23:26:41 +0900

    JAVA - 변수(형변환, 상수, 상수화)

---
+++ 따즈아 JAVA/JV변수상수화.java
@@ -0,0 +1,14 @@
+package JV변수상수;
+
+public class JV변수상수화 {
+
+	public static void main(String[] args) {
+		int a = 3;
+		a = 4;
+		
+		final double PI = 3.14; //final 붙이면 변수를 바꾸지 않겠다. 그리고 변수명을 대문자로 사용.
+		//PI = 3.15; 바꿀수 없음(오류)
+
+	}
+
+}
