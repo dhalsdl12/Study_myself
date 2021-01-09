a5a2b95426bd678888fd29ca5080090e5269893d
ohmin <71929898+dhalsdl12@users.noreply.github.com>
2021-01-09 23:38:10 +0900

    JAVA - 기타 연산자

---
+++ 따즈아 JAVA/JV연산자기타.java
@@ -0,0 +1,16 @@
+package JV연산자3;
+
+public class JV연산자기타 {
+
+	public static void main(String[] args) {
+		int x = 10;
+		x = x+10;
+		
+		System.out.println(x);
+		
+		int y = 10;
+		y += 10;
+		System.out.println(y);
+	}
+
+}
